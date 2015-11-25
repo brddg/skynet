aa4f832fb2558c08302c2092a3220af00892dae2
John Pinkerton <jpinkerton88@gmail.com>
2015-11-25 11:49:39 -0500

    update client side validation

--- wp-content/themes/skynet/js/scripts.js
+++ wp-content/themes/skynet/js/scripts.js
@@ -1,8 +1,8 @@
 (function ($, root, undefined) {
-	
-	$(function () {
-		
-		'use strict';
+
+  $(function () {
+
+    'use strict';
     function getUrlVars(thing) {
       var vars = {};
       var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -14,39 +14,82 @@
     if(getUrlVars('thanks') == 'true') {
       $('#thanks').show();
     }
-		
-		$('#scroll-me').on('click', function(e){
+
+    $('#scroll-me').on('click', function(e){
       e.preventDefault();
       $('html, body').animate({
         scrollTop: $("article").offset().top
       }, 500);
     });
-        
+
     $('#menu-item-19').hover(function(){
       $(this).find('ul.sub-menu').addClass('active');
     }, function(){
       $(this).find('ul.sub-menu').removeClass('active');
     });
-    
+
     $('#menu-item-19').on('click', function(e){
       e.preventDefault();
     });
-    
+
     $('#menu-item-19 ul.sub-menu a').on('click', function(e){
       window.location = $(this).attr('href');
     });
-    
+
+    $('#menu-item-35').on('click', function(e){
+      e.preventDefault();
+      $('#contact-modal').modal();
+    });
+
     $('.close-and-open-contact').on('click', function(e){
       e.preventDefault();
       var $currentModal = $('.modal.in');
       $currentModal.modal('hide');
       setTimeout(function(){
         var $contactModal = $('#contact-modal');
-        $contactModal.modal('show');        
+        $contactModal.modal('show');
       }, 500);
 
-    });   
-		
-	});
-	
+    });
+
+    $('#contact-form').on('submit', function(){
+      var name = $('#name').val();
+      var phone = $('#phone').val();
+      var email = $('#email').val();
+      var zip = $('#zip').val();
+      var recaptcha = $('#g-recaptcha-response').val();
+
+      if (name === "") {
+        $('#name').parent().addClass('has-error');
+        $('#name').focus();
+        return false;
+      }
+      if (phone === "") {
+        $('#phone').parent().addClass('has-error');
+        $('#phone').focus();
+        return false;
+      }
+      if (email === "") {
+        $('#email').parent().addClass('has-error');
+        $('#email').focus();
+        return false;
+      }
+      if (zip === "") {
+        $('#zip').parent().addClass('has-error');
+        $('#zip').focus();
+        return false;
+      }
+      if (recaptcha === "") {
+        $('#recaptcha-help-text').show();
+        return false;
+      }
+      return true;
+    });
+
+    $('#name, #phone, #email, #zip').on('change', function(event){
+      $(event.target).parent().removeClass('has-error');
+    });
+
+  });
+
 })(jQuery, this);
