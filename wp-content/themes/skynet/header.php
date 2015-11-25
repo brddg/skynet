99ae0bf332ce2a87ef10bddc494f11b0a4038378
John Pinkerton <jpinkerton88@gmail.com>
2015-11-25 11:52:21 -0500

    Add recaptcha script to head

--- wp-content/themes/skynet/header.php
+++ wp-content/themes/skynet/header.php
@@ -12,13 +12,17 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+
+    <meta name="google-site-verification" content="FnspWHb54Had-HMe9ZTk9IBoBmHFNLHNo5wTU9yYr0k" />
+
+
 
 		<?php wp_head(); ?>
-
+		
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 	</head>
 	<body <?php body_class(); ?>>
-    
+
     <div id="thanks" class="alert alert-success alert-dismissible" role="alert">
       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
